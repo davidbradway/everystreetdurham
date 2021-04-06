975a9a1ab2510bb903c3533db4e5fc1ae5d0dd35
David Bradway <dpb6@duke.edu>
2021-04-06 14:38:47 -0400

    refactor code and push an updated map

--- process.py
+++ process.py
@@ -18,22 +18,20 @@ def main(directory = 'garminactivities', keepdirectory = 'citystrides'):
     files = os.listdir(os.path.join('data', directory))
     for filename in files:
         try:
-            if not os.path.exists(os.path.join('data', keepdirectory, filename)):
-                # skip anything not gpx
-                if filename.endswith(".gpx"):
-                    with open(os.path.join('data', directory, filename), 'r') as f_in:
-                        gpx = gpxpy.parse(f_in)
+            if not os.path.exists(os.path.join('data', keepdirectory, filename)) and filename.endswith(".gpx"):
+                with open(os.path.join('data', directory, filename), 'r') as f_in:
+                    gpx = gpxpy.parse(f_in)
 
-                    # there may not be any tracks - exception
-                    track = gpx.tracks[0]
+                # there may not be any tracks - exception
+                track = gpx.tracks[0]
 
-                    if 'run' in track.type or 'walk' in track.type or 'other' in track.type:
-                        segment = track.segments[0]
-                        point = segment.points[0]
+                if 'run' in track.type or 'walk' in track.type or 'other' in track.type:
+                    segment = track.segments[0]
+                    point = segment.points[0]
 
-                        if close_to_city(point):
-                            print(f"{filename} \t {track.type} in City. Copy")
-                            copy(os.path.join('data', directory, filename), os.path.join('data', keepdirectory, filename))
+                    if close_to_city(point):
+                        print(f"{filename} \t {track.type} in City. Copy")
+                        copy(os.path.join('data', directory, filename), os.path.join('data', keepdirectory, filename))
         except Exception as e: # work on python 3.x
             # print(str(e))
             pass
