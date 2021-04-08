17272fdf8ce5a5d8cfb1ae23fa246cc590b8f130
David Bradway <dpb6@duke.edu>
2021-04-08 14:38:09 -0400

    add file caching, comments

--- gpx_reduce_all.py
+++ gpx_reduce_all.py
@@ -4,7 +4,8 @@ def main(directory = 'citystrides', keepdirectory = 'reduced'):
     files = os.listdir(os.path.join('data', directory))
     for filename in files:
         try:
-            if not os.path.exists(os.path.join('data', keepdirectory, filename)) and filename.endswith(".gpx"):
+            if not os.path.exists(os.path.join('data', keepdirectory, filename)):
+                # Use -p flag below to plot the before and after, not good for large batch job.
                 # os.system("python gpx_reduce.py -d 10 -n 60 -t 60 -m 200 -v 0 -p -c -2 -o "+ os.path.join('data', keepdirectory, filename) + " "+os.path.join('data', directory, filename))
                 os.system("python gpx_reduce.py -d 10 -n 60 -t 60 -m 200 -v 0 -c -2 -o "+ os.path.join('data', keepdirectory, filename) + " "+os.path.join('data', directory, filename))
         except Exception as e: # work on python 3.x
--- process.py
+++ process.py
@@ -2,6 +2,7 @@ import os
 from shutil import copy
 import gpxpy
 import gpxpy.gpx
+import pickle
 
 '''
  This script copies GPX files that meet certain criteria, like sport type and location
@@ -16,25 +17,41 @@ def close_to_city(point, city_latitude=35.994034, city_longitude=-78.898621, thr
 
 def main(directory = 'garminactivities', keepdirectory = 'citystrides'):
     files = os.listdir(os.path.join('data', directory))
+
+    # use local pickle cache file to avoid rechecking
+    if os.path.exists('.checked.pkl'):
+        with open('.checked.pkl', 'rb') as f:
+            checked = pickle.load(f)
+        print("Cache loaded, ", end="")
+    else:
+        checked = []
+
     for filename in files:
-        try:
-            if not os.path.exists(os.path.join('data', keepdirectory, filename)) and filename.endswith(".gpx"):
-                with open(os.path.join('data', directory, filename), 'r') as f_in:
-                    gpx = gpxpy.parse(f_in)
+        if filename not in checked:
+            try:
+                if not os.path.exists(os.path.join('data', keepdirectory, filename)) and filename.endswith(".gpx"):
+                    with open(os.path.join('data', directory, filename), 'r') as f_in:
+                        gpx = gpxpy.parse(f_in)
 
-                # there may not be any tracks - exception
-                track = gpx.tracks[0]
+                    # there may not be any tracks - exception
+                    track = gpx.tracks[0]
 
-                if 'run' in track.type or 'walk' in track.type or 'other' in track.type:
-                    segment = track.segments[0]
-                    point = segment.points[0]
+                    if 'run' in track.type or 'walk' in track.type or 'other' in track.type:
+                        segment = track.segments[0]
+                        point = segment.points[0]
 
-                    if close_to_city(point):
-                        print(f"{filename} \t {track.type} in City. Copy")
-                        copy(os.path.join('data', directory, filename), os.path.join('data', keepdirectory, filename))
-        except Exception as e: # work on python 3.x
-            # print(str(e))
-            pass
+                        if close_to_city(point):
+                            print(f"{filename} \t {track.type} in City. Copy")
+                            copy(os.path.join('data', directory, filename), os.path.join('data', keepdirectory, filename))
+            except Exception as e: # work on python 3.x
+                # print(str(e))
+                pass
+
+    checked = files
+    with open('.checked.pkl', 'wb') as f:
+        pickle.dump(checked, f)
+    print("saved.")
+
 
 if __name__ == '__main__':
     main()
