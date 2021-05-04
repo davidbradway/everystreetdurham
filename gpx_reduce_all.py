7c4029ef2133ae02a1840fbd35dceb7df351f57d
David Bradway <dpb6@duke.edu>
2021-05-04 10:58:15 -0400

    update my script to call gpx_reduce submodule

--- gpx_reduce_all.py
+++ gpx_reduce_all.py
@@ -11,7 +11,7 @@ def main(directory='citystrides', keepdirectory='reduced'):
                 # Use -p flag below to plot the before and after, not good for large batch job.
                 # os.system("python gpx_reduce.py -d 10 -n 60 -t 60 -m 200 -v 0 -p -c -2 -o " +
                 #         os.path.join('data', keepdirectory, filename) + " "+os.path.join('data', directory, filename))
-                os.system("python gpx_reduce.py -d 10 -n 60 -t 60 -m 200 -v 0 -c -2 -o " +
+                os.system("python gpx_reduce/gpx_reduce.py -d 10 -n 60 -t 60 -m 200 -v 0 -c -2 -o " +
                           os.path.join('data', keepdirectory, filename) + " "+os.path.join('data', directory, filename))
         except Exception as e:  # work on python 3.x
             print(str(e))
